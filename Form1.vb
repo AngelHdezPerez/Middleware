619ccded31d3d798582bf8aeba1069ed8708e3c5
Angel Enrique Hernandez Perez <angel.hernandez85@unach.mx>
2023-10-13 11:07:57 -0600

    Aplicacion Middleware

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim ObjWsEjeMiddleware As New WsEjeMiddleware.Service
+        Dim DsX As New DataSet
+
+        DsX = ObjWsEjeMiddleware.FnListarRegistros
+        Me.DGVPersonas.DataSource = DsX.Tables("Personas")
+    End Sub
+End Class
